8d9618d3104c14c23bc61fa9cf082612aa569a42
Gabriela Castillo <gabrielacastillovas@gmail.com>
2020-07-04 08:54:55 -0500

    reto2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-Vue.component("item-card", {
+Vue.component("listar", {
     data: function () {
       return {
           videos: this.videos2
@@ -8,17 +8,47 @@ Vue.component("item-card", {
         videos2: Array,
     },
     template: `
-        <div class="container">
-            <div class="item" v-for="v in videos2">
-                <div class="item__header">
-                    <img v-bind:src="v.imagen" alt="">
+        <div>
+            <p class="title">Lista de videos</p>
+            <div class="container">
+                
+                <div class="item" v-for="v in videos2">
+                    <div class="item__header">
+                        <img v-bind:src="v.imagen" alt="">
+                    </div>
+                    <div class="item__content">
+                        <h5>{{v.titulo}}</h5>
+                        <span>{{v.visualizaciones}} visualizaciones</span>
+                        <p>{{v.descripcion}}</p>
+                        <button>Ver Detalle</button>
+                    </div>
                 </div>
-                <div class="item__content">
-                    <h5>{{v.titulo}}</h5>
-                    <span>{{v.visualizaciones}} visualizaciones</span>
-                    <p>{{v.descripcion}}</p>
-                    <button>Ver Detalle</button>
-                </div>
+            </div>
+        </div>
+      `,
+  });
+
+  Vue.component("agregar", {
+    data: function () {
+      return {
+          video: {}
+      };
+    },
+    props: {
+        videos2: Array,
+    },
+    template: `
+        <div>
+            <h4 class="title">Agregar Video</h4>
+            <hr>
+            <div class="inputs">
+                <input type="text" placeholder="Titulo" v-model="video.titulo">
+                <input type="text" placeholder="url-video" v-model="video.imagen">
+            </div>
+            <textarea cols="30" rows="10" placeholder="descripcion" v-model="video.descripcion"></textarea>
+            <div class="buttons">
+                <button @click="$emit('value', video)">Agregar</button>
+                <button>Cancelar</button>
             </div>
         </div>
       `,
@@ -27,10 +57,11 @@ Vue.component("item-card", {
 const app = new Vue({
     el: "#app",
     data: {
+        currentPage: "listar",
         videos: []
     },
     created () {
-        this.fetchData()
+        this.fetchData();
     },
     methods: {
         fetchData() {
@@ -42,6 +73,16 @@ const app = new Vue({
                 console.log(data);
                 this.videos = data;    
             });
+        },
+        agregar: function() {
+            console.log("aa");
+            this.currentPage = "agregar";
+        },
+        addVideo(value) {
+            console.log("value", value);
+            value.visualizaciones = 23;
+            this.videos.push(value);
+            this.currentPage = "listar";
         },
     },
 });
